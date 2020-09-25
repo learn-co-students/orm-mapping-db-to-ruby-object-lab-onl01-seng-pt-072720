e72d102663af99a0edad3c4a2f8e3846ccd06518
fentyhall <fentymhall@gmail.com>
2020-09-25 13:59:20 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,7 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
+    # create a new Student object given a row from the database
     new_student = self.new 
     new_student.id = row[0]
     new_student.name = row[1]
@@ -10,6 +11,8 @@ class Student
   end
 
   def self.all
+    # retrieve all the rows from the Students database
+    # remember each row should be a new instance of the Student class
     sql = <<-SQL
       SELECT *
       FROM students
@@ -44,7 +47,7 @@ class Student
     end 
   end 
 
-  def self.first_X_students_in_grade_10(x)
+  def self.first_X_students_in_grade_10(num)
     sql = <<-SQL
       SELECT *
       FROM students
@@ -52,7 +55,7 @@ class Student
       LIMIT ?
     SQL
 
-    DB[:conn].execute(sql, x).map do |row|
+    DB[:conn].execute(sql, num).map do |row|
       self.new_from_db(row)
     end 
   end 
@@ -70,19 +73,21 @@ class Student
     end.first
   end 
 
-  def self.all_students_in_grade_X(x)
+  def self.all_students_in_grade_X(num)
     sql = <<-SQL
       SELECT *
       FROM students
       WHERE grade = ?
     SQL
 
-    DB[:conn].execute(sql, x).map do |row|
+    DB[:conn].execute(sql, num).map do |row|
       self.new_from_db(row)
     end
   end 
 
   def self.find_by_name(name)
+    # find the student in the database given a name
+    # return a new instance of the Student class
     sql = <<-SQL
       SELECT *
       FROM students
